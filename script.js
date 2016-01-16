72acc8791f7b444afbfcee3a7e9f9846855d2dd2
Eryc Duhart <erycduhart009@gmail.com>
2016-01-15 23:44:22 -0500

    a couple functions added in javascript
    can't directly query for desired page in javascript using jquery yet

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-var rawData;
-var source="http://www.powerball.com/powerball/winnums-text.txt";
+var rawData="FAILUE";
+var source="http://www.powerball.com/powerball/winnums-text.txt&callback=?";
 
+function start()
+{
+		ScrapeData();
+		ParseData();
+		InjectData();
+}
 function ScrapeData()
 {
+	jQuery.get(source, function(data) {
+	rawData = data;});
+}
+function ParseData()
+{
 	
+}
+function InjectData()
+{
+	var pageElement = document.getElementById("FilteredContent");
+	alert(pageElement);
+	//pageElement.innerHTML=rawData;
 }
